31b8b86c3badb7e36b51016bba821f7da6736e78
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-03-17 20:28:10 -0400

    Add files via upload

--- HFJS/lexical-scope.js
+++ HFJS/lexical-scope.js
@@ -2,14 +2,53 @@
 var justAVar = "Oh, don't you worry about it, I'm GLOBAL";
 
 
-function whereAreYou() {
-    // local variable inside the function
+function whereAreYou1() {
+    // this function defines a new lexical scope, in which we have a local variable
+    // when called, this function will look in the nearest function scope, if it can't find
+    // the variable locally it will then look in the global scope
     var justAVar = "Just an every day LOCAL";
 
     return justAVar; // return the local variable value
 }
 
-// take the result of invoking the function and store it in variable result
-var result = whereAreYou();
+// assign the reference of the invoked function and store it in variable result
+var result = whereAreYou1();
 // log the result to the console
 console.log(result);
+
+
+// ################################################################
+
+// Now for a nested function example
+
+var justAVar = "Oh, don't you worry about it, I'm GLOBAL";
+
+function whereAreYou2() {
+    var justAVar = "Just an every day LOCAL";
+
+    function inner() {
+        return justAVar;
+    }
+    return inner();
+}
+var innerFunction = whereAreYou2();
+console.log(innerFunction);
+
+// #################################################################
+
+// Now look at this example
+
+var justAVar = "Oh, don't you worry about it, I'm GLOBAL";
+
+function whereAreYou3() {
+    var justAVar = "Just an every day LOCAL";
+
+    function inner() {
+        return justAVar;
+    }
+    return inner; // no longer invoked
+}
+var innerFunction = whereAreYou3(); // innerFunction variable holds the reference to inner
+var result = innerFunction(); // invoke inner, and store the return justAVar in result variable
+console.log(result); // log the results...
+// console.log(innerFunction());

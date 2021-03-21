5c05f3b001a3ad984be22b596319fe0ab06ea9af
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-03-21 19:55:27 -0400

    Add files via upload

---
+++ Part-2 Fundamentals/Strict-mode.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// mispelled the driverlicense variable on purpose
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive!");

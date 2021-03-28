9f72ba453a570961f8e31b2c1f93a752ddbddea7
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-03-27 23:26:54 -0400

    Add files via upload

--- Part-2 Fundamentals/arrow-functions.js
+++ Part-2 Fundamentals/arrow-functions.js
@@ -8,10 +8,12 @@ console.log(age1);
 
 
 // Arrow functions
+// if one line the return is implicit
 const calcAge2 = birthYear => 2037 - birthYear;
 const age2 = calcAge2(1990);
 console.log(age2);
 
+// if multiple lines for the function, then you must put the return keyword
 const yearsUntilRetirement = (birthYear, firstName) => {
     const age = 2037 - birthYear;
     const retirement = 65 - age;

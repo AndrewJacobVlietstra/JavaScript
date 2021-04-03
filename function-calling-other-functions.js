01053c5a09a0a71b9ef552276a6d0e5a2042771e
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-04-02 23:12:19 -0400

    Add files via upload

---
+++ function-calling-other-functions.js
@@ -0,0 +1,16 @@
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+
+    const applePieces = cutFruitPieces(apples);
+    const orangesPieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangesPieces} pieces of oranges.`;
+    return console.log(juice);
+}
+
+// Call the function / invoke the function with ()
+fruitProcessor(2, 3);

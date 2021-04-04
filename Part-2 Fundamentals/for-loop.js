51dc4509cb6728253ec6716ab557a6c1382add27
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-04-03 20:55:41 -0400

    Add files via upload

---
+++ Part-2 Fundamentals/for-loop.js
@@ -0,0 +1,16 @@
+// for loop keeps running while condition is TRUE
+for (let i = 0; i < 5; i++) {
+    console.log(`Repetition ${i}`);
+}
+
+const andrewArray = [
+    'Andrew',
+    'Vlietstra',
+    2021 - 1996,
+    'Front End Dev',
+    ['Colton', 'Matt', 'Evan']
+];
+
+for (let i = 0; i < andrewArray.length; i++) {
+    console.log(andrewArray[i]);
+}

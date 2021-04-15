29e1d1c9ac83a717151cf0ba44be9e99c08057ae
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-04-15 09:35:05 -0400

    Add files via upload

--- Part-7 Closer Look At Functions/functions-challenge.js
+++ Part-7 Closer Look At Functions/functions-challenge.js
@@ -4,5 +4,21 @@ const poll = {
 
     // This generates [0, 0, 0, 0], covered in next section immediatley invoked function expressions
     answers: new Array(4).fill(0),
+
+    registerNewAnswer: function(choice) {
+        // choice = 0;
+        if (choice > this.options.length - 1 || choice < 0) {
+            return console.log(`That's not one of the answers!`);
+        }
+        else {
+            this.answers[choice] += 1;
+            this.displayResults();
+        }
+    },
+
+    displayResults: function() {
+        console.log(`Poll Results are ${this.answers.join(', ')}`);
+    },
 };
 
+poll.registerNewAnswer(0);

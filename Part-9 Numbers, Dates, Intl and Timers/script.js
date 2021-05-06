4c7a31e8752a3d03b493bad002475a38c07723cd
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-05-06 11:14:15 -0400

    Add files via upload

--- Part-9 Numbers, Dates, Intl and Timers/script.js
+++ Part-9 Numbers, Dates, Intl and Timers/script.js
@@ -397,3 +397,40 @@ console.log(huge + ' is a really big number!');
 // Divisions
 console.log(10n / 3n);
 console.log(10 / 3);
+
+
+
+// CREATING DATES LECTURE
+console.log('--CREATING DATES LECTURE--');
+
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('October 06 2020'));
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+// console.log(new Date(2037, 10, 31));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000)); // converting to milliseconds
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142274980000));
+
+console.log(Date.now()); // current time stamp
+
+future.setFullYear(2040);
+console.log(future);

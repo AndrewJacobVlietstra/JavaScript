2ae0f800a76dfb71d627e411662bb91d7734c51c
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-05-06 12:11:28 -0400

    Add files via upload

--- Part-9 Numbers, Dates, Intl and Timers/script.js
+++ Part-9 Numbers, Dates, Intl and Timers/script.js
@@ -20,10 +20,10 @@ const account1 = {
     '2019-12-23T07:42:02.383Z',
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2021-04-29T14:09:59.604Z',
+    '2021-05-01T17:01:17.194Z',
+    '2021-05-05T23:36:17.929Z',
+    '2021-05-06T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -81,6 +81,27 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function(date) {
+  
+  const calcDaysPassed = (date1, date2) => Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if(daysPassed === 0) return 'Today';
+  if(daysPassed === 1) return 'Yesterday';
+  if(daysPassed <= 7) return `${daysPassed} Days Ago`
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+  
+  
+
+}
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -90,10 +111,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
@@ -465,5 +483,18 @@ future.setFullYear(2040);
 console.log(future);
 
 
-// ADDING DATES TO BANKIST APP LECTURE
-console.log('--ADDING DATES TO BANKIST APP LECTURE--');
+
+// OPERATIONS WITH DATES LECTURE
+console.log('--OPERATIONS WITH DATES LECTURE--');
+
+const future2 = new Date(2037, 10, 19, 15, 23);
+console.log(Number(future2));
+console.log(+future2);
+
+// const calcDaysPassed = (date1, date2) => Math.abs((date2 - date1) / (1000 * 60 * 60 * 24));
+
+// const days1 = calcDaysPassed(
+//   new Date(2037, 3, 14), 
+//   new Date(2037, 3, 24)
+// );
+// console.log(days1);

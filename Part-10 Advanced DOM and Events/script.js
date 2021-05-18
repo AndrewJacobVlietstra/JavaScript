c1f1e08f6656f379cc0d032de811dbcc5b6f6b00
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-05-17 21:57:11 -0400

    Add files via upload

--- Part-10 Advanced DOM and Events/script.js
+++ Part-10 Advanced DOM and Events/script.js
@@ -19,11 +19,12 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-// Replaced for loop with forEach method, you can use forEach on nodelists
+// Replaced "for" loop with forEach method, you can use forEach on nodelists
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 // for (let i = 0; i < btnsOpenModal.length; i++) {
 //   btnsOpenModal[i].addEventListener('click', openModal);
 // }
+
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -33,3 +34,100 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+// LECTURES
+
+// Selecting, Creating and Deleting elements lecture
+console.log('Selecting, Creating and Deleting elements lecture');
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+
+// Creating and inserting elements, .insertAdjacentHTML is one way to insert html
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookie for improved functionality and analytics.';
+message.innerHTML = 'We use cookie for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// message is now a live element in the dom, but it cannot be in two places at once
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true)); // to make a clone and have the element in multiple places
+
+// Other methods of adding dom elements before or after a certain point in the document
+// header.before(message);
+// header.after(message);
+
+
+// Delete Elements
+document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+  message.remove(); // newer more recent method
+  // message.parentElement.removeChild(message); // how this used to be done
+});
+
+
+// Styles, attributes and classes lecture
+console.log('Styles, attributes and classes lecture');
+
+//Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = '70px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful logo';
+
+// Non-standard attributes, anything else
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist');
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('a', 'b', 'c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// Dont Use This
+// logo.className = 'jonas';
+
+
+// Implementing Smooth Scroll lecture
+console.log('Implementing Smooth Scroll lecture');
+

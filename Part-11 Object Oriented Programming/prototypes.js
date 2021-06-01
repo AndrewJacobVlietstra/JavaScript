24e26ac6e8ef26f7a0c32a5749ab0efdc5a88e67
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-06-01 11:03:04 -0400

    Add files via upload

---
+++ Part-11 Object Oriented Programming/prototypes.js
@@ -0,0 +1,26 @@
+console.log('PROTOTYPE LECTURE');
+
+// Prototypes
+console.log(Person.prototype);
+
+
+// Preferred way to give all created objects access to methods
+// Better performance-wise than copying the methods into every object
+// This will be a prototype property/function for all objects created by the Person constructor function
+Person.prototype.calcAge = function() {
+    console.log(2021 - this.birthYear);
+};
+
+jonas.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));

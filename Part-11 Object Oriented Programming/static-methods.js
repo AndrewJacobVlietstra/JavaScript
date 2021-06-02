87b4103e61536453f27af66e14538d89fa1c567d
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-06-02 09:46:17 -0400

    Add files via upload

---
+++ Part-11 Object Oriented Programming/static-methods.js
@@ -0,0 +1,36 @@
+console.log('STATIC METHODS LECTURE');
+
+// from method is attached to the Array constructor,
+// from is in the Array namespace
+console.log(Array.from(document.querySelectorAll('h1')));
+
+// will not work with regular arrays like this
+// console.log([1, 2, 3].from());
+
+// Basically a static method is a method that can only be used with its respective constructor
+// like parseFloat with the Number constructor or from with the Array constructor
+
+class Test {
+    constructor (firstName, age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    // Instance Method, 
+    // will be available to all instanced objects, added to the prototype property of the constructor
+    greet() {
+        return `Why hello there ${this.firstName}`;
+    }
+
+    // Static method, only usable by the constructor / class, not shared to instanced objects
+    static hey() {
+        return `Hey there ✋`; // "this" keyword will point back to Test class if used here
+    }
+};
+
+const andrew = new Test('Andrew', 25);
+
+console.log(andrew);
+console.log(andrew.greet());
+// console.log(andrew.hey()); // will throw error, andrew.hey is not a function
+console.log(Test.hey());

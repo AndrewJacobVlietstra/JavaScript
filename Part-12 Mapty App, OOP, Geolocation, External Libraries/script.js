3cf29e3174d25e3e365c0e9ca3f9ae00c2a6e8ec
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-06-07 11:58:16 -0400

    Add files via upload

--- Part-12 Mapty App, OOP, Geolocation, External Libraries/script.js
+++ Part-12 Mapty App, OOP, Geolocation, External Libraries/script.js
@@ -16,6 +16,18 @@ navigator.geolocation.getCurrentPosition(
         const {latitude} = position.coords;
         const {longitude} = position.coords;
         console.log(`https://www.google.ca/maps/@${latitude},${longitude}`);
+
+        const coords = [latitude, longitude];
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     }, 
     function ErrorCallback() {
         alert('Could not get your position.');

3dd8fbef51541d61611d1056fce19b05645426d8
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-06-09 12:10:08 -0400

    Add files via upload

--- Part-13 Asynchronous JS, Promises, Async, Await and AJAX/script.js
+++ Part-13 Asynchronous JS, Promises, Async, Await and AJAX/script.js
@@ -5,20 +5,12 @@ const countriesContainer = document.querySelector('.countries');
 
 
 ///////////////////////////////////////
+// First AJAX call Lecture
 console.log('First AJAX call Lecture');
 
-const getCountryData = function(country){
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-    request.send();
-    
-    request.addEventListener('load', function(){
-        // console.log(this); //returns object with response property in JSON format
-        const [data] = JSON.parse(this.responseText); // convert JSON back to an object
-        // console.log(data);
-    
-        const html = `
-        <article class="country">
+const renderCountry = function(data, className = ''){
+    const html = `
+        <article class="country ${className}">
             <img class="country__img" src="${data.flag}" />
             <div class="country__data">
                 <h3 class="country__name">${data.name}</h3>
@@ -31,9 +23,46 @@ const getCountryData = function(country){
         `;
         countriesContainer.insertAdjacentHTML('beforeend', html);
         countriesContainer.style.opacity = 1;
+};
+
+const getCountryDataAndNeighbour = function(country){
+    // AJAX Call Country 1
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`); // looking by country name
+    request.send();
+    
+    request.addEventListener('load', function(){
+        // console.log(this); //returns object with response property in JSON format
+        const [data] = JSON.parse(this.responseText); // convert JSON back to an object
+        // console.log(data);
+
+        // Render country 1
+        renderCountry(data);
+
+        // Get neighbour country 2
+        const [neighbour] = data.borders
+        console.log(neighbour);
+        if(!neighbour) return;
+
+        // AJAX Call Country 2
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`); // looking by country code
+        request2.send();
+
+        request2.addEventListener('load', function(){
+            // console.log(this);
+            const data2 = JSON.parse(this.responseText);
+            console.log(data2);
+
+            // Render country 2 on screen
+            renderCountry(data2, 'neighbour');
+        });
     });
 };
 
-getCountryData('canada');
-getCountryData('usa');
-getCountryData('portugal');
+// getCountryDataAndNeighbour('canada');
+// getCountryDataAndNeighbour('usa');
+getCountryDataAndNeighbour('portugal');
+// getCountryDataAndNeighbour('africa');
+
+

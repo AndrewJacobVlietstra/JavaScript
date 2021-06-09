e2f8de79b8492fe91b26779302d5a0beea9854e6
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-06-09 11:19:27 -0400

    Add files via upload

--- Part-13 Asynchronous JS, Promises, Async, Await and AJAX/script.js
+++ Part-13 Asynchronous JS, Promises, Async, Await and AJAX/script.js
@@ -3,5 +3,37 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-///////////////////////////////////////
 
+///////////////////////////////////////
+console.log('First AJAX call Lecture');
+
+const getCountryData = function(country){
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+    request.send();
+    
+    request.addEventListener('load', function(){
+        // console.log(this); //returns object with response property in JSON format
+        const [data] = JSON.parse(this.responseText); // convert JSON back to an object
+        // console.log(data);
+    
+        const html = `
+        <article class="country">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)} Million</p>
+                <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            </div>
+        </article>
+        `;
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+        countriesContainer.style.opacity = 1;
+    });
+};
+
+getCountryData('canada');
+getCountryData('usa');
+getCountryData('portugal');

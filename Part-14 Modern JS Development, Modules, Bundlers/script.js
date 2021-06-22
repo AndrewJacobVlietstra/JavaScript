0c13a1f433a76ab7f7c2dff4fd29dfb0dcae79ab
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-06-22 11:16:27 -0400

    Add files via upload

--- Part-14 Modern JS Development, Modules, Bundlers/script.js
+++ Part-14 Modern JS Development, Modules, Bundlers/script.js
@@ -6,16 +6,17 @@ console.log('Importing Module');
 // console.log(price, qt);
 
 
-// import * as ShoppingCart from './shoppingCart.js';
-// ShoppingCart.addToCart('bread', 5);
-// console.log(ShoppingCart);
+import * as ShoppingCart from './shoppingCart.js';
+ShoppingCart.addToCart('bread', 5);
+console.log(ShoppingCart);
 
 
 
 // include a "type": "module", line in the package.json file to prevent error
 // never copy the node modules folder when copying your project, just use 'npm install' command in terminal
 // to install necessary dependencies from your package.json file
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
 
 const state = {
     cart: [
@@ -32,3 +33,7 @@ state.user.loggedIn = false;
 console.log(stateClone);
 
 console.log(stateDeepClone);
+
+if(module.hot) {
+    module.hot.accept();
+}

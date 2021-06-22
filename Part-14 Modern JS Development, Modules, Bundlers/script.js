650247169f7856f6777daf1d19e91f7084201c41
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-06-22 09:47:33 -0400

    Add files via upload

--- Part-14 Modern JS Development, Modules, Bundlers/script.js
+++ Part-14 Modern JS Development, Modules, Bundlers/script.js
@@ -6,6 +6,27 @@ console.log('Importing Module');
 // console.log(price, qt);
 
 
-import * as ShoppingCart from './shoppingCart.js';
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart);
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart);
+
+
+
+// include a "type": "module", line in the package.json file
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+    cart: [
+        {product: 'bread', quantity: 5},
+        {product: 'bread', quantity: 5},
+    ],
+    user: {loggedIn: true},
+};
+
+const stateClone = Object.assign({}, state); // creates new reference pointing to same object
+const stateDeepClone = cloneDeep(state); // creates whole new object
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);

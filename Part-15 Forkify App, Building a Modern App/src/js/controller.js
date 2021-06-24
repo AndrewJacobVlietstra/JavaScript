c92e74bcdd55cdf52e2f1c4bf9def829cc69fa90
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-06-24 12:26:36 -0400

    Add files via upload

--- Part-15 Forkify App, Building a Modern App/src/js/controller.js
+++ Part-15 Forkify App, Building a Modern App/src/js/controller.js
@@ -11,3 +11,39 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function() {
+  try {
+    // await the response object to return
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      // `https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza`
+      );
+    // await the response object to convert to json
+    const data = await res.json();
+
+    // if response is not ok, throw a new error, throwing error will break us out of the function, similar to return
+    if(!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // re-naming some properties of the recipe object
+    let {recipe} = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    };
+    console.log(recipe);
+  } 
+  
+  // any errors that happen throughout the entire process will be caught here and logged to the console
+  catch (err) {
+    console.error(err);
+  }
+};
+
+showRecipe();

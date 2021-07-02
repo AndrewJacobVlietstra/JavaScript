e8294963f598e69d64efda14f35f2ed0de4744e7
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2021-07-01 22:57:28 -0400

    Add files via upload

--- YDKJS Book/1 - Up and Going/chapter-2.js
+++ YDKJS Book/1 - Up and Going/chapter-2.js
@@ -1,5 +1,7 @@
-a = {b: "c"};
-console.log(typeof a);
+'use strict';
+
+let obj = {b: "c"};
+console.log(typeof obj);
 
 function foo() {
     return 42;
@@ -7,3 +9,23 @@ function foo() {
 foo.bar = 'hello world'; // because functions are objects we can apply properties to them
 console.log(foo);
 console.log([] || {});
+
+function makeAdder(x) {
+    function add(y) {
+        return x + y;
+    }
+
+    return add;
+};
+
+let plusOne = makeAdder(1);
+let plusTen = makeAdder(10);
+
+console.log(plusOne(3));
+
+
+function foobar() {
+    var a = arguments[0] === (void 0) ? 2 : arguments[0];
+    console.log( a );
+}
+foobar();

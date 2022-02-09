9b5d4c00a9d83f7406ebca70db2d2931faf1d778
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2022-02-09 09:51:47 -0500

    Add files via upload

--- promiseExercise.js
+++ promiseExercise.js
@@ -12,7 +12,7 @@ const promiseObj = new Promise((resolve, reject) => {
   .catch((err) => console.error(err))
   .finally(() => console.log("Has finished running..."));
 
-  
+
 // Promise.all, will return the values after all the promises have resolved
 // will fail if just one rejects
 const promise1 = Promise.resolve(3);
@@ -26,3 +26,20 @@ Promise.all([promise1, promise2, promise3])
     console.log(values);
   })
   .catch((err) => console.log(err, "Error happened"));
+
+
+// Turn the above promiseObj into an async function
+// await 1 is equivalent to Promise.resolve(1).then(() => undefined)
+async function asyncFunction(result) {
+    console.log('doing some async action...');
+    let data;
+    if (result) {
+        data = await 'Successful';
+    } else {
+        data = await 'Error!';
+    }
+    console.log(data)
+    return console.log('Has finished running');
+}
+
+asyncFunction(true);

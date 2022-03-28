233f0e8e47c6865abdc0fee8da0077aef3319885
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2022-03-28 11:11:42 -0400

    Add files via upload

--- Algorithms & Data Structures/Andrei's Course/arrays.js
+++ Algorithms & Data Structures/Andrei's Course/arrays.js
@@ -15,3 +15,51 @@ strings.unshift('x');
 strings.splice(2, 0, 'Alien');
 
 console.log(strings);
+
+// Create an array from scratch
+
+class myArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index) {
+    return this.data[index]
+  }
+
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index) {
+    const item = this.data[index];
+    this.shiftItems(index);
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length - 1 ; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+  }
+}
+
+const newArray = new myArray();
+newArray.push('a');
+newArray.push('b');
+newArray.push('c');
+newArray.push('d');
+newArray.push('e');
+newArray.delete(1)
+console.log(newArray);

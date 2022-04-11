ef1aaa288ac8f3131b28f51cd2f22a65e05aba9c
Andrew Vlietstra <46541660+AndrewJacobVlietstra@users.noreply.github.com>
2022-04-11 18:47:21 -0400

    Add files via upload

--- Algorithms & Data Structures/Grokking Algorithms/recursionExercises.js
+++ Algorithms & Data Structures/Grokking Algorithms/recursionExercises.js
@@ -9,4 +9,32 @@ function factorial(number) {
   return number * factorial(number - 1);
 }
 
-console.log(factorial(5));
+// console.log(factorial(5));
+
+
+function sum(low, high) {
+  if (high === low) return low;
+
+  return high + sum(low, high - 1);
+}
+
+// console.log(sum(1, 10));
+// ---------------------------------
+
+const nestedArray = [1, 2, 3, [4, 5, 6], 7, [8, [9, 10, 11, [12, 13, 14]]]];
+
+// Write a function that can traverse an arbitrarily deeply nested array of elements
+function traverse(arr) {
+  arr.forEach((value) => {
+
+    // If the value itself is an array, call traverse on it
+    if (Array.isArray(value)) {
+      return traverse(value);
+    }
+
+    // Else its not an array, so log the value
+    return console.log(value);
+  })
+};
+
+traverse(nestedArray);
